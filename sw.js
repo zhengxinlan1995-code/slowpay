3f4428679444f2a049de1bf3968b3aaec38c55d1
ZhenXinLan <233411543+zhengxinlan1995-code@users.noreply.github.com>
2026-08-30 23:13:55 +0800

    Publish Slowpay PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'slowpay-pages-v1';
+const CORE = ['/slowpay/', '/slowpay/manifest.webmanifest', '/slowpay/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match('/slowpay/'))));
+});
